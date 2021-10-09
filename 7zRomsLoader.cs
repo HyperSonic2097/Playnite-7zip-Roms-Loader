d87f1d3de90a8e952398339de6d197ddd05a4df2
HyperSonic2097 <hypersonic2097@alice.it>
2021-10-09 13:08:46 +0200

    Init Template

---
+++ 7zRomsLoader.cs
@@ -0,0 +1,76 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace 7zRomsLoader
+{
+    public class 7zRomsLoader : Plugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private 7zRomsLoaderSettings settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("c3d56d05-0b81-4e9a-b208-d80b1bbde2b9");
+
+        public 7zRomsLoader(IPlayniteAPI api) : base(api)
+        {
+            settings = new 7zRomsLoaderSettings(this);
+        }
+
+        public override void OnGameInstalled(Game game)
+        {
+            // Add code to be executed when game is finished installing.
+        }
+
+        public override void OnGameStarted(Game game)
+        {
+            // Add code to be executed when game is started running.
+        }
+
+        public override void OnGameStarting(Game game)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameStopped(Game game, long elapsedSeconds)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameUninstalled(Game game)
+        {
+            // Add code to be executed when game is uninstalled.
+        }
+
+        public override void OnApplicationStarted()
+        {
+            // Add code to be executed when Playnite is initialized.
+        }
+
+        public override void OnApplicationStopped()
+        {
+            // Add code to be executed when Playnite is shutting down.
+        }
+
+        public override void OnLibraryUpdated()
+        {
+            // Add code to be executed when library is updated.
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new 7zRomsLoaderSettingsView();
+        }
+    }
+}
